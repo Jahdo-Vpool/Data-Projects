c920bda304077f2806cfda65b354812cba84e498
Jahdo <jahdovpool@gmail.com>
2025-08-20 11:49:03 -0400

    worked on week 2 of couch to coder.

---
+++ Couch_to_coder_2025/session_2/array.js
@@ -0,0 +1,8 @@
+const ingredients = ['beef','noodles'];
+ingredients[2] = 'salt'
+ingredients.push('peper')
+ingredients.push('cheese')
+const lastIngredient = ingredients.pop()
+console.log(ingredients);
+console.log(lastIngredient)
+console.log(ingredients.length)
---
+++ Couch_to_coder_2025/session_2/objects.js
@@ -0,0 +1,11 @@
+const employee = {
+    firstName:'John',
+    lastName:'Row',
+    jobTitle: 'Project Manager',
+    salary: 65000,
+    currentProjects: ['Couch to coder','love book','flappy bird','Zombie Hunter']
+};
+
+employee.salary = 76000
+
+console.log(employee.salary)
